74cfe6164444b9aba8a6f21c79ca0cb11a277fdd
Nelson Costa <nelson.costa85@gmail.com>
2022-07-24 20:28:25 +0200

    Misc improvements

--- sync.sh
+++ sync.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-# Removes empty lines
-cat _site/about/index.html | grep . > docs/about/index.html
-cat _site/gallery/index.html | grep . > docs/gallery/index.html
-cat _site/search/index.html | grep . > docs/search/index.html
-cat _site/tags/index.html | grep . > docs/tags/index.html
+for value in 404.html feed.xml index.html en/404.html en/feed.xml en/index.html
+do
+  cat _site/$value | grep . > docs/$value
+  sed -i '' 's#http://localhost:4000#https://bozettoartgallery.com#g' docs/$value
+done
 
-sed -i '' 's#http://localhost:4000#https://bozettoartgallery.com#g' docs/about/index.html
-sed -i '' 's#http://localhost:4000#https://bozettoartgallery.com#g' docs/gallery/index.html
-sed -i '' 's#http://localhost:4000#https://bozettoartgallery.com#g' docs/search/index.html
-sed -i '' 's#http://localhost:4000#https://bozettoartgallery.com#g' docs/tags/index.html
+for value in about gallery search tags en/about en/gallery en/search en/tags
+do
+  cat _site/$value/index.html | grep . > docs/$value/index.html
+  sed -i '' 's#http://localhost:4000#https://bozettoartgallery.com#g' docs/$value/index.html
+done
 
 for d in _site/gallery/*/ ; do
     dir=${d%*/}
     cat _site/gallery/${dir##*/}/index.html | grep . > docs/gallery/${dir##*/}/index.html
     sed -i '' 's#http://localhost:4000#https://bozettoartgallery.com#g' docs/gallery/${dir##*/}/index.html
+done
+
+for d in _site/en/gallery/*/ ; do
+    dir=${d%*/}
+    cat _site/en/gallery/${dir##*/}/index.html | grep . > docs/en/gallery/${dir##*/}/index.html
+    sed -i '' 's#http://localhost:4000#https://bozettoartgallery.com#g' docs/en/gallery/${dir##*/}/index.html
 done
